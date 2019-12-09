0626da06ca419856830749916394d6a54ec98184
Chris Killpack <chris@chriskillpack.com>
2019-12-09 11:06:54 -0800

    Towards making this a service

--- main.go
+++ main.go
@@ -8,24 +8,77 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/BurntSushi/toml"
+	"github.com/kardianos/service"
 )
 
 type tomlConfig struct {
 	Redirects map[string]string
 }
 
+type program struct{}
+
+var (
+	configFile = flag.String("config", "config.toml", "Path to configuration file")
+	port       = flag.Int("port", 80, "Redirect server port")
+	svcControl = flag.String("service", "", fmt.Sprintf("Service action, from %v", service.ControlAction))
+)
+
+func (p *program) Start(s service.Service) error {
+	log.Printf("***** in the Start")
+	go p.run()
+	return nil
+}
+
+func (p *program) run() {
+	log.Printf("***** in the run")
+}
+
+func (p *program) Stop(s service.Service) error {
+	return nil
+}
+
 func main() {
-	configFile := flag.String("config", "config.toml", "Path to configuration file")
-	port := flag.Int("port", 80, "Redirect server port")
+	svcConfig := &service.Config{
+		Name:        "http-redirector",
+		DisplayName: "HTTP-redirector",
+		Description: "Redirects HTTP traffic on the local LAN",
+	}
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	flag.Parse()
+
+	if *svcControl != "" {
+		svc := strings.ToLower(*svcControl)
+
+		ok := false
+		for _, ca := range service.ControlAction {
+			if ca == svc {
+				ok = true
+				break
+			}
+		}
+		if !ok {
+			log.Fatalf("Unknown service action %s", *svcControl)
+		}
+
+		if err := service.Control(s, svc); err != nil {
+			log.Fatal(err)
+		}
+
+		return
+	}
 
 	log.Printf("Reading config from %s", *configFile)
 	var config tomlConfig
 	if _, err := toml.DecodeFile(*configFile, &config); err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 	log.Printf("Read %d redirects", len(config.Redirects))
 
